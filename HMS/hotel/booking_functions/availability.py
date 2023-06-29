182528b091f468cfd00c439a4a9978121013a6a8
Atakashi <petrencko0607@gmail.com>
2023-06-30 00:54:45 +0500

    Added type annotation

--- HMS/hotel/booking_functions/availability.py
+++ HMS/hotel/booking_functions/availability.py
@@ -1,7 +1,8 @@
 import datetime
-from hotel.models import Booking
+from hotel.models import Booking, Room
 
-def check_availability(room, check_in, check_out):
+
+def check_availability(room:Room, check_in:datetime, check_out:datetime) -> bool:
     booking_list = Booking.objects.filter(room=room, check_out__gte=datetime.datetime.now())
     for booking in booking_list:
         if booking.check_in <= check_out and booking.check_out >= check_in:
